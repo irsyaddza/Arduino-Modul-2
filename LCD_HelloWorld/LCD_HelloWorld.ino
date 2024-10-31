02dc578cc90243b523f2b1add174325e49469b3d
irsyaddza <irsyadza04@gmail.com>
2024-10-31 20:52:03 +0700

    Update LCD_HelloWorld.ino

--- LCD_HelloWorld/LCD_HelloWorld.ino
+++ LCD_HelloWorld/LCD_HelloWorld.ino
@@ -1,13 +1,13 @@
-#include <Adafruit_LiquidCrystal.h>
+#include <LiquidCrystal_I2C.h>
+LiquidCrystal_I2C lcd(0x27,16,2);  
 
-Adafruit_LiquidCrystal lcd(0);
-void setup() {
-  
-  lcd.begin(16, 2);
-  lcd.setBacklight(HIGH);
-  lcd.setCursor(3, 0);
-  lcd.print("HELLO WORLD");
-}
+void setup() 
+{   
+lcd.init();
+lcd.backlight(); 
+lcd.setCursor(2,0); 
+lcd.print("Hello world!");  
+} 
 
-void loop() {
+void loop() {   
 }
